3a3fe28af6faa5fc9abd35323852eb60f06b885d
unknown <denisedubois93@gmail.com>
2023-06-23 22:38:15 -0300

    se sumo accion disparar y pegar

--- player.py
+++ player.py
@@ -2,15 +2,21 @@ from doctest import FAIL_FAST
 import pygame
 from constantes import *
 from auxiliar import Auxiliar
+from balas import *
+from enemigo import Enemigo
 
 class Player:
     def __init__(self,x,y,speed_walk,speed_run,gravity,jump_power,jump_height,frame_rate_ms,move_rate_ms) -> None:
-        self.walk_r = Auxiliar.getSurfaceFromSpriteSheet("C:\\Users\\Denise\\Downloads\\CLASE_19_inicio_juego\\CLASE_19_inicio_juego\\Assets\\walk.png",10,1)[:]
-        self.walk_l = Auxiliar.getSurfaceFromSpriteSheet("C:\\Users\\Denise\\Downloads\\CLASE_19_inicio_juego\\CLASE_19_inicio_juego\\Assets\\walk.png",10,1,True)[:]
-        self.stay_r = Auxiliar.getSurfaceFromSpriteSheet("C:\\Users\\Denise\\Downloads\\CLASE_19_inicio_juego\\CLASE_19_inicio_juego\\Assets\\idle_new.png",11,1)
-        self.stay_l = Auxiliar.getSurfaceFromSpriteSheet("C:\\Users\\Denise\\Downloads\\CLASE_19_inicio_juego\\CLASE_19_inicio_juego\\Assets\\idle_new.png",11,1, True)
-        self.jump_r = Auxiliar.getSurfaceFromSpriteSheet("C:\\Users\\Denise\\Downloads\\CLASE_19_inicio_juego\\CLASE_19_inicio_juego\\Assets\\jump_newt.png",12,2,False)
-        self.jump_l = Auxiliar.getSurfaceFromSpriteSheet("C:\\Users\\Denise\\Downloads\\CLASE_19_inicio_juego\\CLASE_19_inicio_juego\\Assets\\jump_newt.png",12,2,True)
+        character_width = CHARACTER_ANCHO
+        character_height = CHARACTER_ALTO
+        self.walk_r = Auxiliar.getSurfaceFromSpriteSheet("C:\\Users\\Denise\\Downloads\\CLASE_19_inicio_juego\\CLASE_19_inicio_juego\\Assets\\walk.png",10,1,character_width,character_height)[:]
+        self.walk_l = Auxiliar.getSurfaceFromSpriteSheet("C:\\Users\\Denise\\Downloads\\CLASE_19_inicio_juego\\CLASE_19_inicio_juego\\Assets\\walk.png",10,1,character_width,character_height,True)[:]
+        self.stay_r = Auxiliar.getSurfaceFromSpriteSheet("C:\\Users\\Denise\\Downloads\\CLASE_19_inicio_juego\\CLASE_19_inicio_juego\\Assets\\idle_new.png",11,1,character_width,character_height)
+        self.stay_l = Auxiliar.getSurfaceFromSpriteSheet("C:\\Users\\Denise\\Downloads\\CLASE_19_inicio_juego\\CLASE_19_inicio_juego\\Assets\\idle_new.png",11,1,character_width,character_height, True)
+        self.jump_r = Auxiliar.getSurfaceFromSpriteSheet("C:\\Users\\Denise\\Downloads\\CLASE_19_inicio_juego\\CLASE_19_inicio_juego\\Assets\\jump_newt.png",12,2,character_width,character_height,False)
+        self.jump_l = Auxiliar.getSurfaceFromSpriteSheet("C:\\Users\\Denise\\Downloads\\CLASE_19_inicio_juego\\CLASE_19_inicio_juego\\Assets\\jump_newt.png",12,2,character_width,character_height,True)
+        self.hit_r = Auxiliar.getSurfaceFromSpriteSheet("C:\\Users\\Denise\\Downloads\\CLASE_19_inicio_juego\\CLASE_19_inicio_juego\\Assets\\hit_hand_1.png",12,1,character_width,character_height,False)
+        self.hit_l = Auxiliar.getSurfaceFromSpriteSheet("C:\\Users\\Denise\\Downloads\\CLASE_19_inicio_juego\\CLASE_19_inicio_juego\\Assets\\hit_hand_1.png",12,1,character_width,character_height,True)
         self.direction = DIRECTION_L
         self.frame = 0
         self.lives = 5
@@ -20,6 +26,7 @@ class Player:
         self.speed_walk =  speed_walk
         self.speed_run =  speed_run
         self.gravity = gravity
+        self.attack_value = 1
         self.jump_power = jump_power
         self.animation = self.stay_r
         self.image = self.animation[self.frame]
@@ -33,9 +40,11 @@ class Player:
         self.move_rate_ms = move_rate_ms
         self.y_start_jump = 0
         self.jump_height = jump_height
-        self.rect_corrected = pygame.Rect(self.rect.x+130,self.rect.y-160 + self.rect.h-50,self.rect.w/3,160 )
-        self.rect_ground_collition = pygame.Rect(self.rect.x+130,self.rect.y + self.rect.h-50,self.rect.w/3,GROUND_RECT_H )
-
+        self.rect_corrected = pygame.Rect(self.rect.x+100,self.rect.y-100 + self.rect.h-50,self.rect.w/3,120 )
+        self.rect_ground_collition = pygame.Rect(self.rect.x+130,self.rect.y+10 + self.rect.h-50,self.rect.w/6,GROUND_RECT_H )
+        self.bullets = []
+        self.is_shooting = False
+        self.hitting = False
 
     def walk(self,direction):
             if(self.direction != direction or (self.animation != self.walk_r and self.animation != self.walk_l)):
@@ -76,31 +85,80 @@ class Player:
             self.move_y = 0
             self.frame = 0
 
-    def do_movement(self,delta_ms,lista_plataformas):
+    def do_movement(self, delta_ms, lista_plataformas):
         self.tiempo_transcurrido_move += delta_ms
-        if(self.tiempo_transcurrido_move >= self.move_rate_ms):
-            if(abs(self.y_start_jump)- abs(self.rect.y) > self.jump_height and self.is_jump):
+        if self.tiempo_transcurrido_move >= self.move_rate_ms:
+            if abs(self.y_start_jump) - abs(self.rect.y) > self.jump_height and self.is_jump:
                 self.move_y = 0
 
             self.tiempo_transcurrido_move = 0
             self.set_x(self.move_x)
             self.set_y(self.move_y)
 
-            if(self.is_on_platform(lista_plataformas) == False):
+            if not self.is_on_platform(lista_plataformas) and self.rect.y < GROUND_LEVEL:
                 self.set_y(self.gravity)
-            elif(self.is_jump): 
-                self.jump(False)
+            elif self.is_jump:
+                if self.is_on_platform(lista_plataformas):
+                    self.jump(False)
+                else:
+                    self.set_y(self.gravity)
 
-    def is_on_platform(self,lista_plataformas):
-        retorno = False
-        if(self.rect.y >= GROUND_LEVEL):
-            retorno = True
+    def is_on_platform(self, lista_plataformas):
+        if self.rect.y == 540:
+            return True
+
+        for plataforma in lista_plataformas:
+            if self.rect_ground_collition.colliderect(plataforma.rect_ground_collision):
+                return True
+
+        return False
+    
+    def hit_enemy(self, enemigo):
+        if self.animation == self.hit_r or self.animation == self.hit_l:  # Verificar si el jugador está realizando un golpe
+            if self.rect_corrected.colliderect(enemigo.rect):  # Verificar colisión entre el jugador y el enemigo
+                return True
+        return False
+    
+    def hurting_enemy(self, lista_enemigos):
+        for enemigo in lista_enemigos:
+            if self.hit_enemy(enemigo):
+                if enemigo.health > 0:
+                    enemigo.hurt(self.attack_value)
+                    if enemigo.health <= 0:
+                        enemigo.animation = enemigo.dead_animation
+                        pygame.time.set_timer(pygame.USEREVENT, 2000)
+                        pygame.event.post(pygame.event.Event(pygame.USEREVENT, {'enemy': enemigo}))
+
+
+
+    def hit(self, on_off):
+        if (on_off == True and self.hitting == False):
+            if (self.direction == DIRECTION_R):
+                self.animation = self.hit_r
+            else:
+                self.animation = self.hit_l
+            self.frame = 0
+            self.hitting = True
         else:
-            for plataforma in lista_plataformas:
-                if(self.rect_ground_collition.colliderect(plataforma.rect_ground_collition)):
-                    retorno = True
-                    break
-        return retorno
+            self.hitting = False
+            self.animation = self.stay()
+#HOLA
+    def shoot(self, on_off):
+        if (on_off and not self.is_shooting):
+            self.is_shooting = True
+            
+            bullet = Bullet(self.rect_corrected.centerx, self.rect_corrected.centery, self.direction)
+            self.bullets.append(bullet)
+        if not on_off:
+            self.is_shooting = False
+
+        self.update_bullets()
+
+    def update_bullets(self):
+        for bullet in self.bullets[:]:
+            bullet.update()
+
+
 
     def set_x(self, delta_x):
         self.rect.x += delta_x
@@ -123,16 +181,28 @@ class Player:
 
 
 
-    def update(self,delta_ms, lista_plataformas):
+    def update(self,delta_ms, lista_plataformas,lista_enemigos):
         self.do_movement(delta_ms, lista_plataformas)
         self.do_animation(delta_ms)
+        self.hurting_enemy(lista_enemigos)
+        if self.is_shooting:
+            self.shoot(True)
+        
+        self.update_bullets()
+    
+    def draw_bullets(self, screen):
+        if self.is_shooting:
+            for bullet in self.bullets:
+                bullet.draw(screen)
         
     
     def draw(self,screen):
         if DEBUG:
-            pygame.draw.rect(screen,RED,self.rect_corrected)
+            pygame.draw.rect(screen, RED, self.rect_corrected)
             pygame.draw.rect(screen, GREEN, self.rect_ground_collition)
         self.image = self.animation[self.frame]
-        screen.blit(self.image,self.rect)
+        screen.blit(self.image, self.rect)
+        self.draw_bullets(screen)
+
 
 
