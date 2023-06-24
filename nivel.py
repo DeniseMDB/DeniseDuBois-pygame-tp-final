e6eb578e211a5aa8deea515cafce20229ef73cdd
unknown <denisedubois93@gmail.com>
2023-06-23 22:43:00 -0300

    pequeñas modificaciones

---
+++ nivel.py
@@ -0,0 +1,6 @@
+#json con cant enemigos nivel 1, 2 ,3
+#boss o no
+#lista con coordenadas de plataforma
+#clase Nivel recibe por parametro el numero de nivel
+#diccionario por nivel con diccionarios de coordenadas, personajes etc
+#append plataformas, enemigos, update, lista items salud
